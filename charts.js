673a992e20b0ab155951a30b1d6f909b2f791286
Julien Cougnaud <julien.cougnaud@etu.univ-nantes.fr>
2020-01-07 22:59:40 +0100

    Update

--- charts.js
+++ charts.js
@@ -1,70 +1,57 @@
+function load_chart_caract_hf(){
+
 // Chargement des données
 var data;
 
-Papa.parse('https://raw.githubusercontent.com/JulienC85/dataviz/master/data_light.csv', {
+Papa.parse('https://julienc85.github.io/dataviz/data_comparaison_caract_h_f.csv', {
   header: true,
   download: true,
   dynamicTyping: true,
   complete: function(results) {
     //console.log(results);
     data = results.data;
-
-    let all_competences_values = [];
-    let all_competences_names = []
     
     let competences_names = ["Endurance","Strength","Power","Speed","Agility","Flexibility","Nerve","Durability","Hand-Eye Coordination","Analytical Aptitude"];
     
-    let nb_women = 0;
-    let nb_men = 0;
+    let averages_women = {"Endurance":0, "Strength":0,"Power":0,"Speed":0,"Agility":0,"Flexibility":0,"Nerve":0,"Durability":0,"Hand-Eye Coordination":0,"Analytical Aptitude":0, "Nombre d'enregistrements":0};
+    let averages_men = {"Endurance":0,"Strength":0,"Power":0,"Speed":0,"Agility":0,"Flexibility":0,"Nerve":0,"Durability":0,"Hand-Eye Coordination":0,"Analytical Aptitude":0, "Nombre d'enregistrements":0};
 
-    let averages_women = new Array(10).fill(0);
-    let averages_men = new Array(10).fill(0);
-    let averages = new Array(10).fill(0);
-    console.log(data.length);
     for (let index = 0; index < data.length; index++) {
         
         const element = data[index];
-        
-        averages[0] = parseFloat(element.Endurance) || 0;
-        averages[1] = parseFloat(element.Strength) || 0;
-        averages[2] = parseFloat(element.Power) || 0;
-        averages[3] = parseFloat(element.Speed) || 0;
-        averages[4] = parseFloat(element.Agility) || 0;
-        averages[5] = parseFloat(element.Flexibility) || 0;
-        averages[6] = parseFloat(element.Nerve) || 0;
-        averages[7] = parseFloat(element.Durability) || 0;
-        averages[8] = parseFloat(element["Hand-Eye Coordination"]) || 0;
-        averages[9] = parseFloat(element["Analytical Aptitude"]) || 0;
 
         if (element.Sex === "F") {
-          for (let i = 0; i < averages_women.length; i++) {
-            averages_women[i] += averages[i];
-          }
-
-            nb_women += 1;
+          averages_women[element["Noms de mesures"]] += element["Valeurs de mesures"];
         } else {
-          for (let i = 0; i < averages_women.length; i++) {
-            averages_men[i] += averages[i];
-          }
-          nb_men += 1;
-        }
+            averages_men[element["Noms de mesures"]] += element["Valeurs de mesures"];
+        }      
     }
 
-    averages_women = averages_women.map(x => x / nb_women);
-    averages_men = averages_men.map(x => x / nb_men);
-    console.log(averages_women)
+    let nb_women = averages_women["Nombre d'enregistrements"];
+    let nb_men =  averages_men["Nombre d'enregistrements"];
+        
+    competences_names = competences_names.filter(item => item !== "Nombre d'enregistrements")
 
-    console.log(averages_men)
+    let averages_women_array = [];
+    let averages_men_array = [];
+
+    competences_names.forEach(element => {
+        averages_women[element] /= nb_women;
+        averages_men[element] /= nb_men;
+        averages_women_array.push(averages_women[element]);
+        averages_men_array.push(averages_men[element]);
+    });
+
     var trace1 = {
         x: competences_names,
-        y: averages_women,
+        y: averages_women_array,
         name: 'Women',
         type: 'bar'
       };
       
       var trace2 = {
         x: competences_names,
-        y: averages_men,
+        y: averages_men_array,
         name: 'Men',
         type: 'bar'
       };
@@ -76,11 +63,135 @@ Papa.parse('https://raw.githubusercontent.com/JulienC85/dataviz/master/data_ligh
         barmode: 'group'
       };
       
-      Plotly.newPlot('graphe_hf_caract', data_to_print, layout);
-
-    
-      
+      Plotly.newPlot('graphe_hf_caract', data_to_print, layout);      
   }
 });
+}
 
+function load_chart_sport_hf(){
+
+  // Chargement des données
+  var data;
+  
+  Papa.parse('https://julienc85.github.io/dataviz/data_comparaison_sports_h_f.csv', {
+    header: true,
+    download: true,
+    dynamicTyping: true,
+    complete: function(results) {
+      data = results.data;
+      console.log(data);
+      
+      let sports_names = [];
+      
+      let nb_women = {};
+      let nb_men = {};
+  
+      for (let index = 0; index < data.length; index++) {
+          
+          const element = data[index];
+
+          if (element.Sex === "F") {
+            if(element.Sport in nb_women){
+              nb_women[element.Sport] += element.Nb;
+            }
+            else{
+              nb_women[element.Sport] = element.Nb;
+              if(sports_names.indexOf(element.Sport) == -1){
+                sports_names.push(element.Sport);
+              }
+            }
+          } else {
+              if(element.Sport in nb_men){
+                nb_men[element.Sport] += element.Nb;
+              }
+              else{
+                nb_men[element.Sport] = element.Nb;
+                if(sports_names.indexOf(element.Sport) == -1){
+                  sports_names.push(element.Sport);
+                }
+              }
+          }      
+      }
+    
+      let nb_women_array = [];
+      let nb_women_array_reverse = [];
+      let nb_men_array = [];
+
+  
+      sports_names.forEach(element => {
+        nb_women_array.push(nb_women[element] || 0);
+        nb_women_array_reverse.push(-nb_women[element] || 0);
+
+        nb_men_array.push(nb_men[element] || 0);
+      });
+
+      console.log(nb_women_array);
+      console.log(nb_men_array)
+  
+      trace1 = {
+        uid: '9f2de8e2-01e2-44cf-9597-d8c9d17a223a', 
+        meta: {columnNames: {
+            x: 'Men, x', 
+            y: 'Men, y; Women, y'
+          }}, 
+        name: 'Men', 
+        type: 'bar', 
+        x: nb_men_array, 
+        y: sports_names, 
+        marker: {color: 'powderblue'}, 
+        hoverinfo: 'x', 
+        orientation: 'h'
+      };
+      trace2 = {
+        uid: '31653fd0-228e-4932-88af-340740cd1dea', 
+        meta: {columnNames: {
+            x: 'Women, x', 
+            y: 'Men, y; Women, y', 
+            text: 'text'
+          }}, 
+        name: 'Women', 
+        type: 'bar', 
+        x: nb_women_array_reverse, 
+        y: sports_names, 
+        marker: {color: 'seagreen'}, 
+        text: nb_women_array, 
+        hoverinfo: 'text', 
+        orientation: 'h'
+      };
+      let data_to_print = [trace1, trace2];
+      let layout = {
+        xaxis: {
+          type: 'linear', 
+          range: [-10000, 10000], 
+          title: {text: 'Number of people'}, 
+          ticktext: [-10000, -7500, -5000, -2500, 0, 2500, 5000, 7500, 10000], 
+          tickvals: [-10000, -7500, -5000, -2500, 0, 2500, 5000, 7500, 10000]
+        }, 
+        yaxis: {
+          /*type: 'linear',*/ 
+          range: [0, 50], 
+          title: {text: 'Sport'}, 
+          autorange: false
+        },
+        bargap: 0.1, 
+        barmode: 'relative', 
+        autosize: false,
+        width: 1000,
+        height:1500,
+        margin: {
+          l: 200,
+          r: 50,
+          b: 100,
+          t: 100,
+          pad: 4
+        }
+      };
+
+        
+      Plotly.newPlot('graphe_hf_sport', data_to_print, layout);    
+    }
+  });
+  }
+
+load_chart_sport_hf();
 
